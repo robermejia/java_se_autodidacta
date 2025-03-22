2dca9a28a3f0bee2635d4660c4f4e0b8050bc721
Roberto Mejía <59416763+robermejia@users.noreply.github.com>
2025-03-21 20:06:34 -0500

    Estructura (temas) Java SE

---
+++ src/main/java/com/robermejia/variables/Intro.java
@@ -0,0 +1,22 @@
+package com.robermejia.variables;
+// ================== SINTAXIS:===========================
+// TipoDato nombreVariable;
+// Ejemplos: Usuario user, Integer num,  
+// ====================================================
+// Nota: var es una caracteristica de inferencia 
+//(el compilador deduce el tipo de variable basándose 
+// en su inicialización) desde la version 10 de 
+// tipos no tipado dinámico.
+
+// ============= CATEGORÍA DE TIPOS: =====================
+// - Primitivos: enteros, flotantes, booleanos, etc.
+// - Referencia: arreglos, clases, Interfaces, etc.
+
+// ======================= REGLAS ========================
+// 1. Empiezan con letra minúscula
+// 2. No puede ser el mismo que una palabra clave
+// 3. No deben tener el mismo nombre que otras variables en el mismo bloque.
+// 4. Si esta compuesta por mas por varias palabras, lleva mayuscula al 
+//    inicio de cada palabra desde lña segunda palabra.
+public class Intro {
+}
