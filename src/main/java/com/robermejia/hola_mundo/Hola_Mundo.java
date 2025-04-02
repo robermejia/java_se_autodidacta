b0762cf1c66ab29eeb98ead13e68c6f9754a0919
Roberto Mejía <robertomejiacollazos@gmail.com>
2025-04-02 17:16:55 -0500

    Estructura de todo el repositorio, es decir todos los temas de JAVA SE

--- src/main/java/com/robermejia/hola_mundo/Hola_Mundo.java
+++
@@ -1,8 +0,0 @@
-package com.robermejia.hola_mundo;
-
-public class Hola_Mundo {
-    public static void main(String[] args) {
-        /* Hay una vieja leyenda que dice que si empiezas a programar con un HolaMundo te ir&aacute; bien. */
-        System.out.println("¡Hola, Mundo!");
-    }
-}
